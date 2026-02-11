30d9c4bdc9ea911cedfcc6c62ec761b408496b38
perennialcrow <halgurdze@gmail.com>
2026-02-10 22:53:56 -0800

    Implement the Actor class.

--- PPO/Actor.py
+++ PPO/Actor.py
@@ -9,8 +9,22 @@ responsible for step 1 only
 This should have 372 input nodes,
 and 12 ouput nodes"""
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 class Actor:
-    def __init__(self):
-        pass
+    def __init__(self, state_dim = 372, action_dim = 12, lr = 1e-4):
+        self.network = nn.Sequential(
+            nn.Linear(state_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, action_dim),
+            nn.Tanh()
+        )
+        
+        self.optimizer = optim.Adam(self.network.parameters(), lr=lr)
 
+    def forward(self, x):
+        return self.network(x)
