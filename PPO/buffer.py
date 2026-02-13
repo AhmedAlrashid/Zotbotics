536e84b86676de80b7c969296b2ac17a8d5d6595
Anirudh523 <aniaar523@gmail.com>
2026-02-13 15:20:07 -0800

    finished rtg

--- PPO/buffer.py
+++ PPO/buffer.py
@@ -4,36 +4,49 @@ and also for sampling the transitions, and also for calculating the advantages a
 This will be in charge of step 4 and 5 
 """
 import torch
+import torch.nn as nn
 import numpy as np
 
 class Buffer:
-    def __init__(self, gamma = 0.99):
+    def __init__(self, gamma = 0.99, batch_size = 12):
         # gamma is discount factor
         self.states = []
         self.actions = []
         self.rewards = []
         self.logProbs = []
         self.values = [] # values from critic network
+        self.rtg = None
         self.gamma = gamma
+        self.current_state = None
+        self.actor = Actor()
 
     def store(self, state, action, reward, logProb, value):
         self.states.append(state)
         self.actions.append(action)
         self.rewards.append(reward)
-        self.logProbs.append(logProbs)
+        self.logProbs.append(logProb)
         self.values.append(value)
         
 
-    # def sample(self):
-    #     pass
+    def sample(self):
+        
+
 
     def calculate_rtg(self):
-        # rtg = r_t + gamma * r_t+1
+        #rewards = torch.tensor(self.rewards, dtype=torch.float32)
+        rtg = []
+        rewards_calculation = 0
+        for i in range(len(self.rewards)):
+            rewards_calculation += self.rewards[i] * self.gamma**i
+            rtg.append(rewards_calculation)
+
+        rtg = torch.tensor(rtg, dtype=torch.float32)
+        self.rtg = rtg
         # rewardsToGo = a tensor
-        return rewardsToGo
+        return rtg
 
     def calculate_advantages(self):
          # A = rewardsToGo - values
         values = torch.tensor(self.values, dtype=torch.float32)
-        advantages = rewardsToGo - values
+        advantages = self.rtg - values
         return advantages
